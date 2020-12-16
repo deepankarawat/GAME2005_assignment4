6ac0afbf3ece3c6ba139157e5df2c9133602e568
deepankarawat <56966784+deepankarawat@users.noreply.github.com>
2020-12-15 23:14:11 -0500

    slider added
    slidewr added

---
+++ game 2005 deepankar/Assets/_Scripts/SliderTextScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SliderTextScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public string textPrefix;
+    public string textSuffix;
+    public Text sliderText;
+    void Start()
+    {
+        //sliderText = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void updateText(float value)
+    {
+        sliderText.text = textPrefix + " " + value.ToString("F2") + " " + textSuffix;
+    }
+}
